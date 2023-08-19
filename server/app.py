380edddb97e456b9653bc97d9c5d72d857824edb
Christina Lambiase <christina.lambiase@gmail.com>
2023-08-19 09:05:21 -0700

    complete lab

--- server/app.py
+++ server/app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, make_response, request
 from flask_cors import CORS
 from flask_migrate import Migrate
 from models import Cheese, Producer, db
+from flask_restful import Api, Resource, abort
 
 # from flask_restful import Api, Resource
 
@@ -11,18 +12,86 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.json.compact = False
 
+
 CORS(app)
 migrate = Migrate(app, db)
 
 db.init_app(app)
 
-# api = Api(app)
+api = Api(app)
 
 
-@app.route("/")
-def index():
-    response = make_response({"message": "Hello Fromagers!"}, 200)
-    return response
+class Producers(Resource):
+    def get(self):
+        producers = [p.to_dict(rules=("-cheeses",)) for p in Producer.query.all()]
+        return make_response(producers, 200)
+
+
+api.add_resource(Producers, "/producers")
+
+
+class ProducerById(Resource):
+    def get(self, id):
+        producer = Producer.query.get_or_404(
+            id, description="Resource not found"
+        ).to_dict()
+        return make_response(producer, 200)
+
+    def delete(self, id):
+        producer = Producer.query.get_or_404(id, description="Resource not found")
+        db.session.delete(producer)
+        db.session.commit()
+        return make_response("", 204)
+
+
+api.add_resource(ProducerById, "/producers/<int:id>")
+
+
+class Cheeses(Resource):
+    def post(self):
+        data = request.get_json()
+        try:
+            producer_id = data.get("producer_id")
+            producer = Producer.query.get_or_404(producer_id)
+            new_cheese = Cheese(
+                kind=data["kind"],
+                is_raw_milk=data["is_raw_milk"],
+                production_date=data["production_date"],
+                image=data["image"],
+                price=data["price"],
+                producer_id=producer.id,
+            )
+        except Exception as e:
+            abort(406, errors=["validation errors"])
+        db.session.add(new_cheese)
+        db.session.commit()
+        return make_response(new_cheese.to_dict(), 201)
+
+
+api.add_resource(Cheeses, "/cheeses")
+
+
+class CheeseById(Resource):
+    def patch(self, id):
+        data = request.get_json()
+        try:
+            cheese = Cheese.query.get_or_404(id)
+            for attr, value in data.items():
+                setattr(cheese, attr, value)
+        except Exception as e:
+            abort(406, errors=["validation errors"])
+        db.session.add(cheese)
+        db.session.commit()
+        return make_response(cheese.to_dict(rules=("-producer",)), 202)
+
+    def delete(self, id):
+        cheese = Cheese.query.get_or_404(id, description="Resource not found")
+        db.session.delete(cheese)
+        db.session.commit()
+        return make_response("", 204)
+
+
+api.add_resource(CheeseById, "/cheeses/<int:id>")
 
 
 if __name__ == "__main__":
--- server/models.py
+++ server/models.py
@@ -2,14 +2,36 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy.orm import validates
 from sqlalchemy_serializer import SerializerMixin
+from datetime import datetime
 
 db = SQLAlchemy()
 
 
 class Producer(db.Model, SerializerMixin):
     __tablename__ = "producers"
+    serialize_rules = ("-cheeses.producer",)
 
     id = db.Column(db.Integer, primary_key=True)
+    founding_year = db.Column(db.Integer)
+    name = db.Column(db.String, nullable=False)
+    region = db.Column(db.String)
+    operation_size = db.Column(db.String)
+    image = db.Column(db.String)
+
+    cheeses = db.relationship("Cheese", backref="producer", cascade="delete")
+
+    @validates("founding_year")
+    def validate_year(self, key, year):
+        if not year >= 1900:
+            raise ValueError("Founding year must be after 1900")
+        return year
+
+    @validates("operation_size")
+    def validate_size(self, key, operation_size):
+        size = ["small", "medium", "large", "family", "corporate"]
+        if operation_size not in size:
+            raise ValueError("Invalid operation size")
+        return operation_size
 
     def __repr__(self):
         return f"<Producer {self.id}>"
@@ -17,8 +39,28 @@ class Producer(db.Model, SerializerMixin):
 
 class Cheese(db.Model, SerializerMixin):
     __tablename__ = "cheeses"
+    serialize_rules = ("-producer.cheeses",)
 
     id = db.Column(db.Integer, primary_key=True)
+    kind = db.Column(db.String)
+    is_raw_milk = db.Column(db.Boolean)
+    production_date = db.Column(db.DateTime)
+    image = db.Column(db.String)
+    price = db.Column(db.Float)
+    producer_id = db.Column(db.Integer, db.ForeignKey("producers.id"))
+
+    @validates("production_date")
+    def validate_date(self, key, date):
+        production_date = datetime.strptime(date, "%Y-%m-%d")
+        if production_date >= datetime.now():
+            raise ValueError("Production date must be in the past")
+        return production_date
+
+    @validates("price")
+    def validate_price(self, key, price):
+        if not 1.00 < price < 45.00:
+            raise ValueError("Price must be between $1.00 and $45.00")
+        return price
 
     def __repr__(self):
         return f"<Cheese {self.id}>"
